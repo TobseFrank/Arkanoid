ff2c04cb5132ff86c8bc43adb8c66411b9c53fda
Tobias Frank <Tobiasfr1@yahoo.de>
2024-03-25 14:28:03 +0100

    Paddle behaviour depending on Paddle scale

--- Assets/Scripts/GamePaddle.cs
+++ Assets/Scripts/GamePaddle.cs
@@ -9,22 +9,26 @@ public class GamePaddle : MonoBehaviour
     private GameObject ammo;
 
     [SerializeField]
+    private GameObject rightBorder;
+
+    [SerializeField]
+    private GameObject leftBorder;
+
+    [SerializeField]
     private float speed;
-
-    private float width;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //width = transform.position.Scale();
-        //this.GetComponent<Transform>().position = new Vector3(3,0,0);
-    }
 
     // Update is called once per frame
     void Update()
     {   
         float dir = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(dir * speed * Time.deltaTime,0,0);
+        //transform.position += new Vector3(dir * speed * Time.deltaTime,0,0);
+
+        float xMax = (rightBorder.transform.position.x - (rightBorder.transform.localScale.x * 0.5f)) - transform.localScale.x * 0.5f;
+        float xMin = (leftBorder.transform.position.x + (leftBorder.transform.localScale.x * 0.5f))+ transform.localScale.x * 0.5f;
+
+        float xPlayerNew = transform.position.x + dir * Time.deltaTime * speed;
+        float xActual = Mathf.Clamp(xPlayerNew, xMin, xMax);
+        transform.position = new Vector3(xActual, transform.position.y, transform.position.z);
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             Instantiate(ammo, new Vector3(transform.position.x - 0.5f,transform.position.y,0),transform.rotation);
